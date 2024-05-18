40c40924846de3633ca52e1af1c5da3b24ce41cc
tayyabaawan642 <157307322+tayyabaawan642@users.noreply.github.com>
2024-05-18 21:31:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+let userName;
+
+let myName = "Tayyaba Awan";
+
+let message = "Hello world";
+alert(message);
+
+let stdName = "John Doe";
+let stdAge = 15;
+let stdCourse = "Web Development";
+alert(stdName);
+alert(stdAge);
+alert(stdCourse);
+
+let dish = "PIZAA" + "\n" + "PIZZ" + "\n" + "PIZ" + "\n" + "PI" + "\n" + "P";
+alert(dish);
+
+let email ="My email address is example@example.com ";
+alert(email);
+
+let book = "A smater way to learn JavaScript";
+alert("I am trying to learn from the book " + book);
+
+let str = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(str);
+
+document.write("Yah! I can write HTML content through JavaScript.");
